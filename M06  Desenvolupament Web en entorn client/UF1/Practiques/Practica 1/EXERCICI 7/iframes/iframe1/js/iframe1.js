a0f85c4bc35ccbde81db7d80c6e74c58d40e0547
Jordi Rocha <jordirocharocha@gmail.com>
2021-10-21 19:35:14 +0200

    Update on js files

--- M06  Desenvolupament Web en entorn client/UF1/Practiques/Practica 1/EXERCICI 7/iframes/iframe1/js/iframe1.js
+++ M06  Desenvolupament Web en entorn client/UF1/Practiques/Practica 1/EXERCICI 7/iframes/iframe1/js/iframe1.js
@@ -2,11 +2,38 @@ window.addEventListener('DOMContentLoaded', function () {
     document.getElementById("btnCal").addEventListener("click", function () {
         let name = document.getElementById("name").value;
         let tickets = document.getElementById("ticket").value;
-        var select = document.getElementById('show');
-        var value = select.options[select.selectedIndex].value;
-        
-        window.parent.document.getElementById("iframe2").contentWindow.document.getElementById("price").innerText = tickets * value;
-        parent.document.getElementById("estado").innerText = "Pendent de l’acceptació del preu";
-        window.parent.document.getElementById("estado").style.width = "50%";
+        let select = document.getElementById('show');
+        let value = select.options[select.selectedIndex].value;
+
+        let boolName = false;
+        let boolTickets = false;
+        let boolShow = false;
+
+        if (name == "") {
+            document.getElementById("nameInput").innerText = "Name cannot be empty";
+        } else {
+            boolName = true;
+            document.getElementById("nameInput").innerText = "";
+        }
+
+        if (value == "" || value == "Choose") {
+            document.getElementById("selectInput").innerText = "Select a show";
+        } else {
+            boolShow = true;
+            document.getElementById("selectInput").innerText = "";
+        }
+
+        if (isNaN(tickets) || tickets == "") {
+            document.getElementById("ticketInput").innerText = "Introduce a quantity of tickets";
+        } else {
+            boolTickets = true;
+            document.getElementById("ticketInput").innerText = "";
+        }
+
+        if (boolName && boolShow && boolTickets) {
+            window.parent.document.getElementById("iframe2").contentWindow.document.getElementById("price").innerText = tickets * value;
+            parent.document.getElementById("estado").innerText = "Pendent de l’acceptació del preu";
+            window.parent.document.getElementById("estado").style.width = "50%";
+        }
     });
 });
